791a839cf828778846c1a46cff5cb2c2b9423a62
nina <67192642+quynhu-d@users.noreply.github.com>
2023-06-22 18:09:38 +0300

    Update trajectories.py

--- lib/trajectories/trajectories.py
+++ lib/trajectories/trajectories.py
@@ -1,5 +1,7 @@
 import numpy as np
 from tqdm.auto import tqdm, trange
+import glob
+
 
 def corpus_length(corpus_path):
     corpus_l = 0
@@ -8,9 +10,17 @@ def corpus_length(corpus_path):
             corpus_l += 1
     return corpus_l
 
+def trajectory_from_text(text, wdict, wdim, text_length_threshold=None):
+    text = line.split()
+    if (text_length_threshold is not None) and (len(text) < text_length_threshold):
+        return None
+    ts = np.array([wdict[w][:wdim] for w in text if w in wdict])
+    return ts
+
 def get_corpus_trajectories(
     corpus_path="../../STB/DATA/english_data/english_newlit_corpus.txt",
     wdict_path="../../STB/DATA/english_data/english_newlit_SVD_dict.npy",
+    corpus_saved_as_file=True,
     nrows=None, text_length_threshold=None, wdim=None, svd_descending=False,
     clip=True
 ):
@@ -20,17 +30,28 @@ def get_corpus_trajectories(
             wdict = {w:e[::-1] for w, e in wdict.items()}
     except:
         print(f'No word embedding dictionary found at path {wdict_path}.')
-    corpus_len = corpus_length(corpus_path)
-    with open(corpus_path, 'r', encoding='utf-8') as f:
+    if corpus_saved_as_file:
+        corpus_len = corpus_length(corpus_path)
+        with open(corpus_path, 'r', encoding='utf-8') as f:
+            time_series = []
+            for line in tqdm(f, total=corpus_len, desc='Reading corpus...'):
+                text = line.split()
+                if (nrows is not None) and (len(time_series) == nrows):
+                    break
+                ts = trajectory_from_text(text, wdict, wdim, text_length_threshold)
+                if ts is not None:
+                    time_series.append(ts)
+    else:
         time_series = []
-        for line in tqdm(f, total=corpus_len, desc='Reading corpus...'):
-            text = line.split()
+        for file in sorted(glob.glob(corpus_path)):
+            with open(file, 'r', encoding='utf-8') as f:
+                text = f.read().split()
             if (nrows is not None) and (len(time_series) == nrows):
                 break
-            if (text_length_threshold is not None) and (len(text) < text_length_threshold):
-                continue
-            ts = np.array([wdict[w][:wdim] for w in text if w in wdict])
-            time_series.append(ts)
+            ts = trajectory_from_text(text, wdict, wdim, text_length_threshold)
+            if ts is not None:
+                time_series.append(ts)
+        
     if clip:
         min_len = min(list(map(len, time_series)))
         return np.array([ts[:min_len] for ts in time_series])
